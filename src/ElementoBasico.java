1a85789e827712bc184166f17354da6405ccf353
Lavínia Winter <lavinia_000@outlook.com>
2023-06-15 17:10:25 -0300

    Mudei o tamanho da imagem

--- src/ElementoBasico.java
+++ src/ElementoBasico.java
@@ -18,7 +18,7 @@ public abstract class ElementoBasico extends JButton {
         this.tabuleiro = tabuleiro;
 
         this.imagem = Tabuleiro.createImageIcon(iconPath);
-        this.setIcon(resize(this.imagem, 50, 40));
+        this.setIcon(resize(this.imagem, 70, 50));
         // this.setBorder(BorderFactory.createBevelBorder(0,Color.RED, Color.BLACK ));
         this.setMargin(new Insets(0, 0, 0, 0));
     }
